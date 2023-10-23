f365f7b9d1d2efd699596cab6856b17d79eae474
DINKAR202 <Dinkar202@outlook.com>
2023-10-24 00:58:20 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+// import React from 'react';
+// import { View, Text } from 'react-native';
 
-function App(): JXS.Element{
-  return(
-    <View>
-      <Text>Hi</Text>
-      <Text>Hi</Text>
-      <Text>Hi</Text>
-      <Text>Hi</Text>
-      <Text>Hi</Text>
-      <Text>Hi</Text>
-    </View>
-  )
-}
+// function App(): JSX.Element{
+//   return(
+//     <View>
+//       <Text>Hi</Text>
+//       <Text>Hi</Text>
+//       <Text>Hi</Text>
+//       <Text>Hi</Text>
+//       <Text>Hi</Text>
+//       <Text>Hi</Text>
+//     </View>
+//   )
+// }
 
-export default App;
+// export default App;
---
+++ AppPro.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    useColorScheme
+} from 'react-native';
+
+function AppPro(): JSX.Element{ 
+    return(
+        <View>
+            <Text>Hello</Text>
+            <Text>Hello</Text>
+            <Text>Hello</Text>
+            <Text>Hello</Text>
+            <Text>Hello</Text>
+        </View>
+    );
+}
+
+export default AppPro;
