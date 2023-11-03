3f917b5d01adee8e0d3f4636eca7cb35f9216af0
DINKAR202 <Dinkar202@outlook.com>
2023-11-03 22:13:13 +0530

    updated commit

--- Learning/App.tsx
+++ Learning/App.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
-import NavBar from './Components/NavBar';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <NavBar />
-      <Text style={{fontSize: 40}}>Hi</Text>
-      <Button title="press here and click this button" />
+    <View style={styles.container}>
+      <Text style={styles.text}>Hi there</Text>
+      <TouchableOpacity
+        style={styles.cursorButton}
+        onPress={() => alert('Cursor button pressed')}>
+        <Text style={styles.buttonText}>Cursor</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 54,
+    marginBottom: 20,
+  },
+  cursorButton: {
+    backgroundColor: 'blue',
+    padding: 20,
+    borderRadius: 15,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
 
 export default App;
