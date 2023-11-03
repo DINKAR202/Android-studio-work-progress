ca8d4185967a8ea6eb747ca4316b9909d8ba4632
DINKAR202 <Dinkar202@outlook.com>
2023-11-03 21:49:43 +0530

    updated commit

--- Learning/App.tsx
+++ Learning/App.tsx
@@ -3,15 +3,13 @@ import {View, Text, Button} from 'react-native';
 import NavBar from './Components/NavBar';
 
 const App = () => {
-  return(
+  return (
     <View>
-        <NavBar/>
-      <Text style={{fontSize:40}}>
-        Hi
-      </Text>
-      <Button title='press here' />
+      <NavBar />
+      <Text style={{fontSize: 40}}>Hi</Text>
+      <Button title="press here" />
     </View>
-  )
-}
+  );
+};
 
 export default App;
--- Learning/Components/NavBar.js
+++ Learning/Components/NavBar.js
@@ -1,15 +1,12 @@
-import React from 'react'
-import {View, Text} from 'react-native'
-
+import React from 'react';
+import {View, Text} from 'react-native';
 
 const NavBar = () => {
   return (
     <View>
-           <Text style={{fontSize:45}}>
-                I'm from NavBar section.
-            </Text> 
+      <Text style={{fontSize: 45}}>I'm from NavBar section.</Text>
     </View>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
