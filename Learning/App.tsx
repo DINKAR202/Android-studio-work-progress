c1fde31bacb0eca8ed7e869e8813931c39372f9e
DINKAR202 <Dinkar202@outlook.com>
2023-11-09 21:15:36 +0530

    updated commit

--- Learning/App.tsx
+++ Learning/App.tsx
@@ -4,27 +4,24 @@ import {View, Text, Button, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <View style={{flex:6}}>
-      <View style={{flex:2, backgroundColor:'red'}}></View>
-      <View style={{flex:1, backgroundColor:'green'}}></View>
-      <View style={{flex:1, backgroundColor:'yellow'}}></View>
+    <View>
+      {/* <View style={{flex: 2, backgroundColor: 'red'}}></View>
+      <View style={{flex: 1, backgroundColor: 'green'}}></View>
+      <View style={{flex: 1, backgroundColor: 'yellow'}}></View>
       {/* <NavBar /> */}
-      <Text style={styles.heading}>Hi</Text>
-      <Button title="Don't press here" />
+      <Text>Hi</Text>
+      {/* <Button title="Don't press here" /> */} */}
     </View>
-
-    
   );
 };
 
-const styles = StyleSheet.create({
-  heading:{
-    fontSize: 30,
-    color: '#00fa9a',
-    backgroundColor:'#800000',
-    marginTop: '100',
-  }
-
-})
+// const styles = StyleSheet.create({
+//   heading: {
+//     fontSize: 30,
+//     color: '#00fa9a',
+//     backgroundColor: '#800000',
+//     marginTop: '100',
+//   },
+// });
 
 export default App;
