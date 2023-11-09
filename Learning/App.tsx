ff862472ff49222913bf66fd4241657c8c242951
DINKAR202 <Dinkar202@outlook.com>
2023-11-09 21:10:02 +0530

    updated commit

--- Learning/App.tsx
+++ Learning/App.tsx
@@ -1,12 +1,30 @@
-import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+// import NavBar from './Components/NavBar';
 
-export default function App() {
+const App = () => {
   return (
-    <NavigationContainer>
-      <Text>Hi</Text>
-    </NavigationContainer>
+    <View style={{flex:6}}>
+      <View style={{flex:2, backgroundColor:'red'}}></View>
+      <View style={{flex:1, backgroundColor:'green'}}></View>
+      <View style={{flex:1, backgroundColor:'yellow'}}></View>
+      {/* <NavBar /> */}
+      <Text style={styles.heading}>Hi</Text>
+      <Button title="Don't press here" />
+    </View>
+
+    
   );
-}
+};
+
+const styles = StyleSheet.create({
+  heading:{
+    fontSize: 30,
+    color: '#00fa9a',
+    backgroundColor:'#800000',
+    marginTop: '100',
+  }
+
+})
 
 export default App;
